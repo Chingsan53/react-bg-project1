f7b0692d6bda68cfb5774ca4cc91bba6c00a8f8f
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-01-02 17:41:27 -0800

    update final for project pizza

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
@@ -74,11 +75,17 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-        {pizzas.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+        <Fragment>
+          <p>
+            Authentic Italian Pizza.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </Fragment>
       ) : (<p>We're still working on our menu. Please come back later :)</p>)}
 
       {/* <Pizza
@@ -100,15 +107,18 @@ function Menu() {
 function Pizza({pizzaObj}) {
   console.log(pizzaObj);
 
-  if(pizzaObj.soldOut) return null;
+  //if(pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div className="">
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        {/* {
+          pizzaObj.soldOut ? <span>SOLD OUT</span> : <span>{pizzaObj.price}</span>
+        } */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
