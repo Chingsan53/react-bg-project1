3f6b18b485c10bd843321d3138ad69e0e4c02fd3
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2024-01-03 20:03:46 -0800

    finish up the interactive app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,69 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import "./App.css";
 
-export default function App() {
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
+const messages = [
+  "Learn React ⚛",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
-  async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice');
-    const data = await res.json();
-    console.log(data.slip.id);
-    setAdvice(data.slip.advice);
-    setCount((c) => c + 1);
-  }
-
-  useEffect(function() {
-    getAdvice();
-  }, []);
-
+const App = () => {
   return (
     <div>
-      <h1>Welcome to the advice portal!</h1>
-      <button onClick={getAdvice}>Get advice</button>
-      <p><strong>Here is the advice: </strong>{advice}</p>
-      <Message count={count}/>
-      
+      <Steps />
+      <Steps />
     </div>
   );
-}
+};
 
-function Message(props) {
+const Steps = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
   return (
-    <p>You have read <strong>{props.count}</strong> of advice(s)</p>
-  )
-}
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
